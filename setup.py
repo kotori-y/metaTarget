eda1101bea8b3cc4fe5957af5ceee890edfede95
kotori yang <yzjkid9@gmail.com>
2021-11-23 23:29:11 +0800

    Added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='meta_target',
+    version='1.1.0',
+    description='',
+    author='Kotori and Catkin',
+    author_email='kotori@cbdd.me',
+    url='https://github.com/kotori-y/metaTarget',
+    packages=["metaTarget"]
+)
