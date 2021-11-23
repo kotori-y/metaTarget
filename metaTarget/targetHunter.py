b01c14c62b12f8b31dcdae968b4f600f43afcdb9
kotori yang <yzjkid9@gmail.com>
2021-11-23 22:18:10 +0800

    Completed target-hunter

---
+++ metaTarget/targetHunter.py
@@ -0,0 +1,76 @@
+import requests
+import re
+import pandas as pd
+
+from requests.exceptions import RequestException
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def targetHunter(smile):
+    # https://www.cbligand.org/TargetHunter/search_target.php
+
+    login_url = 'https://www.cbligand.org/TargetHunter/login.php'
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+              'Connection': 'close',
+              }
+    data = {
+        'usrname': 'catkin7',
+        'usrpasswd': 'y7B5@t!g',
+        'btnLogin2': 'Login',
+    }
+    session = requests.Session()
+
+    try:
+        response = session.post(url=login_url, data=data, headers=header, timeout=60, verify=False)
+    except RequestException:
+        return "{}"
+
+    search_url = 'https://www.cbligand.org/TargetHunter/search_target.php'
+
+    data = {
+        "File1": (None, '', 'application/octet-stream'),
+        "hiddensmi": (None, smile),
+        "Smiles": (None, smile),
+        "Fingerprint": (None, 'FP2'),
+        "searchtype": (None, 'sim2d'),
+        "similarity": (None, '60'),
+        "ChEMBL": (None, 'on'),
+        "PubChem": (None, 'on'),
+        "Submit": (None, 'Submit'),
+    }
+
+    try:
+        page = session.post(search_url, files=data, headers=header, timeout=60).text
+    except RequestException:
+        return "{}"
+
+    patterns = re.compile('(wrk\d*)')
+    try:
+        wrkid = re.findall(patterns, page)[0]
+    except IndexError:
+        return "{}"
+
+    result_url = 'https://www.cbligand.org/TargetHunter/retrieve_target.php?similarity=60&dir=' + wrkid + '&FP=FP2'
+
+    try:
+        result_page = session.get(result_url, headers=header, timeout=60).text
+    except RequestException:
+        return "{}"
+
+    try:
+        df = pd.read_html(result_page, header=0)[1]
+    except ValueError:
+        return "{}"
+
+    clean_target = [x.replace('Find Assays Nearby', '') for x in df['Target']]
+    df['Target'] = clean_target
+
+    return df.T.to_json()
+
+
+if __name__ == "__main__":
+    _smi = "CCCCCC"
+    res = targetHunter(_smi)
+    print(res)
